e2c1a63e9cfc5d73847e18160ead420ae08ba860
Oleksandr Shturmov <oleks@oleks.info>
2016-02-28 15:34:19 +0100

    OS X compatibility

--- initvm.sh
+++ initvm.sh
@@ -5,14 +5,16 @@ set -x
 
 VERNAME=su16vm-v1.0
 
-wget -N "http://napoleon.hiperfit.dk/~oleks/${VERNAME}.ova"
+curl -z "${VERNAME}" -o "${VERNAME}" \
+  "http://napoleon.hiperfit.dk/~oleks/${VERNAME}.ova"
 
 vboxmanage unregistervm "${VERNAME}" --delete
 
 vboxmanage import "${VERNAME}.ova"
 
 function sfmount {
-  vboxmanage sharedfolder add "${VERNAME}" --name "$1" --hostpath "$PWD/$1" --automount
+  vboxmanage sharedfolder add "${VERNAME}" \
+    --name "$1" --hostpath "$PWD/$1" --automount
 }
 
 sfmount "conf"
